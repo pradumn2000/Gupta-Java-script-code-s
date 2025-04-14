6cdeb9b53ccf402ee7d168ae97a7dbe378b075c3
Pradyumn gupta <125115115+pradumn2000@users.noreply.github.com>
2025-04-14 20:37:58 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+//alert (3+3) // we are using nodejs, not browser 
+console.log(3+
+    3);// 
+
+// Primitive Datatypes
+//number=>2 to power 53 certain amount of number are avilable in memory after that we use Bigint
+//bigint
+//String=>""
+//boolean=> true/false
+//null is a standalone value
+//undefined not assign a value 
+//symbol 
+
+//Object 
+console.log(typeof undefined);// type of undefined
+console.log(typeof null);// type of object
