3b906c813f6bb46ce4b6e3b8f64f48a28daebb4e
Pradyumn gupta <125115115+pradumn2000@users.noreply.github.com>
2025-04-15 18:34:54 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33 vhange in number
+//"33abc" => NaN (not a number )
+// true=>1; false=>0
+
+//let isLoggedIn=1
+let isLoggedIn=" hitesh "
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//when we changed 1=> true; 0=> false 
+// when we changed empty value ""=>false
+// when we changed string "hitesh"=> true
+
